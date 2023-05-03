406190568dbe4f3efeef4c8e740df142b14a07a7
096benjaminbenoit <096benjaminbenoit@gmail.com>
2023-05-03 12:08:22 +0200

    register form style

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,24 +3,63 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
-            ->add('email')
+            ->add('firstName', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'John'
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Doe'
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => '0123456789'
+                ],
+                'constraints' => [
+                    new Regex(array(
+                        'pattern' => '/^(0|\+33)[1-9]( *[0-9]{2}){4}$/',
+                        'message' => 'Veuillez saisir un numéro de téléphone valide'
+                    ))
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'john@exemple.fr'
+                ],
+                'constraints' => [
+                    new Email([
+                        'message' => 'Veuillez saisir une adresse mail valide',
+                        'mode' => 'loose'
+                    ])
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -33,20 +72,33 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'placeholder' => '8 caractères minimum'
+            ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit faire au minimum {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
             ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $data = $event->getData();
+            foreach (['firstName', 'lastName'] as $fieldName) {
+                if (isset($data[$fieldName])) {
+                    $data[$fieldName] = ucfirst(strtolower($data[$fieldName]));
+                }
+            }
+            $event->setData($data);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
